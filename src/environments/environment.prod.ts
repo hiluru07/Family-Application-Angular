0ce7fcc4ff43e1df92ecac0e463333eab8712a5f
HILURU MOHAMED <hilurumohamed456@gmail.com>
2025-12-19 18:26:03 +0530

    Update environment files, image URLs and other changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,4 @@ export const environment ={
     production : true,
     API_BASE_URL: 'https://family-application-production.up.railway.app/api',
     IMAGE_BASE_URL: 'https://family-application-production.up.railway.app'
-
 }
